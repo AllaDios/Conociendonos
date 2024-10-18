80988ee3592f10e475a2cee048d921ea51eb423c
AllaDios <lautaroalladio12@gmail.com>
2024-10-18 16:07:44 -0300

    Modifiqué app.py para que escuche en todas las interfaces y use el puerto dinámico

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -5,15 +6,11 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Puedes manejar la lógica de generación del QR aquí
         nombre = request.form.get('nombre')
         instagram = request.form.get('instagram')
         otra_red = request.form.get('otra-red')
         edad = request.form.get('edad')
-
-        # Redirigir a la página de presentación con parámetros
         return render_template('presentacion.html', nombre=nombre, instagram=instagram, otra_red=otra_red, edad=edad)
-    
     return render_template('index.html')
 
 @app.route('/presentacion')
@@ -21,5 +18,5 @@ def presentacion():
     return render_template('presentacion.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    port = int(os.environ.get('PORT', 5000))  # Usa el puerto asignado por Render
+    app.run(host='0.0.0.0', port=port)
